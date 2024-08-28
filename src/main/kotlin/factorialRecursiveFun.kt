d48e02f570353fdbea1cd6842431440ba801a063
priyaaaahere <priyadarshinimohapatra1023@gmail.com>
2024-08-28 22:51:03 +0530

    28/08/24

--- src/main/kotlin/factorialRecursiveFun.kt
+++ src/main/kotlin/factorialRecursiveFun.kt
@@ -1,8 +1,13 @@
 fun factorial(n:Int):Int{
-    return if(n<=1) 1 else n*factorial(n-1)
+    if(n<=1) {
+        return 1
+    }
+    else {
+        return n * factorial(n - 1)
+    }
 }
 fun main(){
-    println("Enter number to find factorial:")
+    println("Enter number to find factorial: ")
     var y:Int= readln().toInt()
     var x=factorial(y)
     println(x)
