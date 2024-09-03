480cddbc99500005833526d38dc2ddc481ec977c
priyaaaahere <priyadarshinimohapatra1023@gmail.com>
2024-09-03 13:06:22 +0530

    02/09/24

---
+++ src/main/kotlin/Inheritance.kt
@@ -0,0 +1,19 @@
+open class Rectangle(val a:Double,val b:Double){
+    fun area():Double{
+        return a * b
+    }
+    open fun display(){
+        println("Area of Rectangle: ${area()}")
+    }
+}
+class Square(side:Double):Rectangle(side,side){
+    override fun display() {
+        println("Area lof Square: ${area()}")
+    }
+}
+fun main(){
+    val myRectangle=Rectangle(4.0,5.0)
+    myRectangle.display()
+    val mySquare=Square(3.0)
+    mySquare.display()
+}
